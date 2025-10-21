28081b3a08d8f1897d8f0bdeeab58f6337a939be
IT22200938 <it22200938@my.sliit.lk>
2025-10-21 22:52:17 +0530

    Init added

---
+++ turtlebot3_autonav/__init__.py
@@ -0,0 +1,11 @@
+"""
+TurtleBot3 Autonomous Navigation Package
+
+This package provides autonomous navigation capabilities for TurtleBot3,
+including SLAM, Nav2 integration, and DQN-based learning.
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Your Name'
+__email__ = 'user@example.com'
+
